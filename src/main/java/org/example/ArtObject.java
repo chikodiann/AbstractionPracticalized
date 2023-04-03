f1da570dcbacea1445e255d6af79360cfffa69cd
chikodiann <chikodiann@gmail.com>
2023-04-03 21:50:55 +0100

    finished up

--- src/main/java/org/example/ArtObject.java
+++ src/main/java/org/example/ArtObject.java
@@ -1,2 +1,15 @@
-package org.example;public class ArtObject {
+package org.example;
+
+public class ArtObject extends ProductForSale {
+
+    public ArtObject(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("This " + type + " is a beautiful reproduction");
+        System.out.printf("This price of the piece is $%6.2f %n", price);
+        System.out.println(description);
+    }
 }
